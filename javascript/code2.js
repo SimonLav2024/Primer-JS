135a467f0f1d136da055ad82fca266abaec66d55
SimonLav2024 <simonlav94@gmail.com>
2024-07-31 11:31:04 +0200

    en progreso

--- javascript/code2.js
+++ javascript/code2.js
@@ -7,21 +7,56 @@ function calcularEdad() {
     
     //console.log(typeof(edad)); //esto es para ver el tipo de variable que es (en este caso es line de texto "string")
     edad = Number(edad);
-
-    //Calcular la edad humana de la mascota
-    let edadHumana = edad * 7;
+    let mensaje = "";
+    //si se cumple lo que esta entre parentesis se ejecuta lo que va entre las llaves
+    if(edad > 0 && edad < 50){
+        //Calcular la edad humana de la mascota
+        let edadHumana = edad * 7;
 
     //Crear el mensaje para mostrar
-    let mensaje = `${nombre} tiene ${edad} años, que equivalen a ${edadHumana} años humanos`;
-
+        mensaje = `${nombre} tiene ${edad} años, que equivalen a ${edadHumana} años humanos`;
+    }else{
+        mensaje = "Los datos introducidos no son correctos";
+    }
     document.getElementById("resultado").textContent = mensaje;
+    
 }
 
 function calcularPrecio() {
     let nomCliente = document.getElementById("nombreCliente").value;
     let precio = document.getElementById("precioSinIVA").value;
+    let cbSelec = document.getElementById("IVAred").checked;
     precio = Number(precio);
-    let precioConIVA = precio + 21%(precio);
-    let mens = `${nomCliente} paga ${precio} euros, que serán ${precioConIVA} euros con IVA`;
+    mens = "";
+    let IVA = 21;
+    let IVAReducido = 10;
+    let importe;
+    
+    if(precio >= 0){
+        if(cbSelec){
+            importe = precio + (precio*IVAReducido/100);
+            mens = `${nomCliente} paga ${precio} euros, que serán ${importe} euros con IVA reducido`;
+        }else{
+            importe = precio + (precio*IVA/100);
+            mens = `${nomCliente} paga ${precio} euros, que serán ${importe} euros con IVA`;
+        }
+    }else{
+        mens = "Los valores introducidos deben ser positivos"
+    }
     document.getElementById("result").textContent = mens;
+}
+
+function calc() {
+    let precioconIVA = document.getElementById("conIVA").value;
+    let cbSelect = document.getElementById("IVA10").checked;
+    precioconIVA = Number(precioconIVA);
+    mens = "";
+    let IVA = 21;
+    
+    if(cbSelect){
+       IVA = 10;
+    }
+    let valFin = precioconIVA - (precioconIVA*IVA/100);
+    mens = `El precio sin IVA es ${valFin}`;
+    document.getElementById("valFin").textContent = mens;
 }
